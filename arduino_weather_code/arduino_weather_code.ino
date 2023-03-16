7da85e30d8d7116cbb409b840f6a716ed4020db3
PyryL <pyry.laaksonen@tuni.fi>
2023-03-16 14:18:15 +0200

    MQTT working

--- arduino_weather_code/arduino_weather_code.ino
+++ arduino_weather_code/arduino_weather_code.ino
@@ -3,7 +3,8 @@
 #include <Ethernet.h>
 #include <PubSubClient.h>
 #include <SPI.h>
-#define outTopic "alykkaat2k23 Topic"
+#define inTopic    "ICT1B_in_2020"                    // * MQTT channel where data are received 
+#define outTopic   "ICT1B_out_2020" 
 #define  mac_6    0x32 
 
 
@@ -19,6 +20,13 @@ const int buttonPins[4] = {A0, A1, A2, A3};//Button pins for keypad inputs
 bool tm = false;
 int buttonState = 0; //Button state of keypad buttons
 
+//These are for debouncing grounding button for analog input
+int gnd_btn = A5;
+int gnd_state = 0;
+int gnd_output = 9;
+unsigned long lastDebounceTime = 0;
+short debounceDelay = 50;
+
 
 volatile int step_counter = 0; //Counter for rising edges of wind speed signal
 volatile int freq = 0; //Frequency of wind speed signal
@@ -26,27 +34,34 @@ volatile int freq = 0; //Frequency of wind speed signal
 int wind_speed = 0;
 int sensor_value = 0;
 
-char alphabet = 65;
-const int MAX_ASCII = 122;
-int a = 0, b = 0;
+
+
+
+EthernetClient ethClient;
+
+char* deviceId = "2023PJS420";
+char* clientId = "8481024";
+char* deviceSecret = "tamk";  
+
+
+
+byte server[] = {10, 6, 0, 21}; //Server IP address
+void callback(char* topic, byte* payload, unsigned int length);
+PubSubClient client (server, 1883, callback, ethClient);
+
+
+
+static uint8_t mymac[6] = { 0x44,0x76,0x58,0x10,0x00,mac_6 }; //Mac address for ethernet module
 
 void testmode();
 void Timer_int_routine();
 void pulse_counter();
-
-EthernetClient ethClient;
-
-
-byte server[] = {10, 6, 0, 20}; //Server IP address
-//void callback(char* topic, byte* payload, unsigned int length);
-//PubSubClient client (server, 1883, callback, ethClient);
-
-//const byte my_ip[] = {192, 168, 112, 89};
-
-static uint8_t mymac[6] = { 0x44,0x76,0x58,0x10,0x00,mac_6 }; //Mac address for ethernet module
+void Connect_MQTT_server();
+void send_MQTT_message();
 
 void setup() {
-
+  pinMode(A6, OUTPUT);
+  pinMode(A5, INPUT);
   attachInterrupt(digitalPinToInterrupt(2), pulse_counter, RISING);
   Timer1.initialize(3000000);
   Timer1.attachInterrupt(Timer_int_routine);
@@ -64,13 +79,12 @@ void setup() {
   lcd.print("Wind speed: ");
   lcd.setCursor(0, 1);
   lcd.print("Direction: ");
-  
 
-  char* deviceId = "2023PJS420";
-  char* clientId = "8481024";
-  char* deviceSecret = "tamk";  
+  fetch_IP();
   
-  //client.connect(clientId, deviceId, deviceSecret);
+  
+  
+  Connect_MQTT_server();
   
 }
 
@@ -92,15 +106,46 @@ void loop() {
     }
   }
 
-  sensor_value = analogRead(A4);
-  float wind_direction = sensor_value*(5/1023.0);
+  
+
+  int gnd_reading = digitalRead(gnd_btn);
+
+  if (gnd_reading != gnd_state){
+    lastDebounceTime = millis();
+  }
+  
+  //For grounding the analog input when there's no input to it
+  if((millis()- lastDebounceTime) > debounceDelay);  
+    if (gnd_state == 0){
+      if (gnd_reading == HIGH){
+        digitalWrite(gnd_output, HIGH);
+        gnd_state = 1;
+      }
+    }
+    else if (gnd_state == 1){
+      if (gnd_reading == HIGH){
+        digitalWrite(gnd_output, LOW);
+        gnd_state = 0;
+      }
+    }   
   
   
   
   lcd.setCursor(12,0);
   lcd.print(wind_speed);
   lcd.print(" m/s ");
-      
+    
+  wind_dir();
+
+
+  send_MQTT_message();
+
+}
+
+void wind_dir(){//Wind direction calculated here
+  sensor_value = analogRead(A4);
+  float wind_direction = sensor_value*(5/1023.0);
+
   if(wind_direction >= 0 && wind_direction < 0.47){
     
     lcd.setCursor(11,1);
@@ -158,8 +203,6 @@ void loop() {
     delay(500);
   }
 
-  //client.publish(outTopic, bufa);
-
 }
 
 
@@ -170,90 +213,24 @@ void testmode(){
     wind_speed = -0.24 + freq * 0.699;
     lcd.setCursor(18, 3);
     lcd.print("TM");
-    lcd.setCursor(0, 0);
-    lcd.print("Wind speed: ");
+    
+    lcd.setCursor(12, 0);
     lcd.print(wind_speed);
     lcd.print(" m/s ");
-    lcd.setCursor(0,1);
-    lcd.print("Direction: ");
+    
 
-    sensor_value = analogRead(A4);
-    float wind_direction = sensor_value*(5/1023.0);
-
-    if(wind_direction >= 0 && wind_direction < 0.47){
-    
-    lcd.setCursor(11,1);
-    /*lcd.print(wind_direction);
-    lcd.setCursor(7,1);*/
-    lcd.print("N ");
-    delay(500);
-  }
-  else if(wind_direction > 0.47 && wind_direction < 0.95){
-    
-    lcd.setCursor(11,1);
-    /*lcd.print(wind_direction);
-    lcd.setCursor(7,1);*/
-    lcd.print("NE");
-    delay(500);
-  }
-  else if(wind_direction > 0.95 && wind_direction < 0.95){
-    
-    lcd.setCursor(11,1);
-    /*lcd.print(wind_direction);
-    lcd.setCursor(7,1);*/
-    lcd.print("E ");
-    delay(500);
-  }
-  else if(wind_direction > 1.43 && wind_direction < 1.9){
-    
-    lcd.setCursor(11,1);
-    /*lcd.print(wind_direction);
-    lcd.setCursor(7,1);*/
-    lcd.print("SE");
-    delay(500);
-  }
-  else if(wind_direction > 1.9 && wind_direction < 2.38){
-    
-    lcd.setCursor(11,1);
-    /*lcd.print(wind_direction);
-    lcd.setCursor(7,1);*/
-    lcd.print("S ");
-    delay(500);
-  }
-  else if(wind_direction > 2.38 && wind_direction < 2.85){
-    
-    lcd.setCursor(11,1);
-    /*lcd.print(wind_direction);
-    lcd.setCursor(7,1);*/
-    lcd.print("SW");
-    delay(500);
-  }
-  else if(wind_direction > 2.85 && wind_direction < 3.33){
-    
-    lcd.setCursor(11,1);
-    /*lcd.print(wind_direction);
-    lcd.setCursor(7,1);*/
-    lcd.print("W ");
-    delay(500);
-  }
-  else if(wind_direction > 3.33){
-    
-    lcd.setCursor(11,1);
-    /*lcd.print(wind_direction);
-    lcd.setCursor(7,1);*/
-    lcd.print("NW");
-    delay(500);
-  }
+    wind_dir();
     
     
     delay(1000);
-    lcd.clear();
+    
   }
 
 }
 
 
 void pulse_counter(){ //Interrupt routine, increments the step counter which counts the rising edges of wind speed signal
+//Runs when there is input to digital pin 2
   step_counter++;
 }
 void Timer_int_routine(){ //This happens every 3 seconds, counts the frequency of wind speed signal
@@ -264,7 +241,57 @@ void Timer_int_routine(){ //This happens every 3 seconds, counts the frequency o
 
 
 
-void running_alphabet(){
+void fetch_IP(){ //Get ip address for ethernet module
+  byte result = 1;
+
+  result = Ethernet.begin( mymac);
+  if (result == 0){
+    lcd.setCursor(0,2);
+    lcd.print("IP fail");
+  }
+  else{
+  lcd.setCursor(0,2);
+  lcd.print(Ethernet.localIP());
+  }
+}
+
+void callback(char* topic, byte* payload, unsigned int length){
+  char* received_topic = topic;
+  byte received_payload = payload;
+  unsigned int received_length = length;
+}
+void Connect_MQTT_server(){
+  if (!client.connected()){                                   // check if allready connected  
+      if (client.connect(clientId, deviceId, deviceSecret)){ // connection to MQTT server 
+        Serial.println(" Connected OK " );
+        client.subscribe(inTopic);                        // subscript to in topic        
+      }
+  }
+}
+
+void send_MQTT_message(){                     // Send MQTT message
+  char bufa[50];                             //  Print message to serial monitor
+  if (client.connected()){ 
+    sprintf(bufa,"Alykkaat MQTT: moi");               // create message with header and data
+    Serial.println( bufa ); 
+    client.publish(outTopic,bufa);                        // send message to MQTT server        
+  }
+  else{                                                           //   Re connect if connection is lost
+    delay(500);
+    Serial.println("No, re-connecting" );
+    client.connect(clientId, deviceId, deviceSecret);
+    delay(1000);                                            // wait for reconnecting
+  }
+}
+
+
+
+
+void running_alphabet(){//Surprise
+  char alphabet = 65;
+  const int MAX_ASCII = 122;
+  int a = 0, b = 0;
+
   while (true){
     while (a < 20){
       if (alphabet == 123){
@@ -331,25 +358,3 @@ void running_alphabet(){
   }
 
 }
-
-
-void fetch_IP(){ //Get ip address for ethernet module
-  byte result = 1;
-
-  result = Ethernet.begin( mymac);
-  if (result == 0){
-    lcd.setCursor(0,2);
-    lcd.print("IP fail");
-  }
-  else{
-  lcd.setCursor(0,2);
-  lcd.print(Ethernet.localIP());
-  }
-}
-
-void callback(char* topic, byte* payload, unsigned int length){
-  char* received_topic = topic;
-  byte received_payload = payload;
-  unsigned int received_length = length;
-}
-
